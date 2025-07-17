351cbe8060e62c371877b58842064fd1c3e41c31
MariliaVitorio <biomariliavitorio@gmail.com>
2025-07-17 08:15:51 -0300

    cenario remover carrinho

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -1,5 +1,5 @@
 describe('Carrinho', () => {
-    it('Adiciona blusa ao carrinho', () => {
+    it('Adiciona produto ao carrinho', () => {
         cy.start()
 
         cy.get('#twotabsearchtextbox').click()
@@ -15,19 +15,44 @@ describe('Carrinho', () => {
 
         cy.get('#productTitle').should('exist');
 
-       // cy.get('#size_name_0 > .a-button-inner > .a-button-input').click()
+ 
         cy.get('#add-to-cart-button').should('be.visible').click({force:true})
      
-       // cy.get('#buy-now-button').click()
-       // cy.get('#sc-buy-box-ptc-button > .a-button-inner > .a-button-input').click()
-
+      
 
       cy.get('#sw-gtc > .a-button-inner > .a-button-text').click()
 
 cy.get('#sc-buy-box-ptc-button > .a-button-inner > .a-button-input').click()
 
-//ecluri
-// cy.get('.sc-action-delete-active > .a-declarative > .a-color-link').click()
-// cy.contains(' foi removido de Carrinho de compras.').should('exist')
+
+    });
+
+        it('Remoer produto do carrinho', () => {
+        cy.start()
+
+        cy.get('#twotabsearchtextbox').click()
+            .type('Alexa');
+
+        cy.get('#nav-search-submit-button').click()
+        
+        cy.get('span.a-color-state.a-text-bold')
+            .should('contain.text', 'Alexa')
+
+        cy.contains('span', 'Echo com Alexa (Última geração): Com som premium e hub Zigbee de casa inteligente - Cor Preta')
+            .should('be.visible').click()
+
+        cy.get('#productTitle').should('exist');
+
+ 
+        cy.get('#add-to-cart-button').should('be.visible').click({force:true})
+     
+      
+
+      cy.get('#sw-gtc > .a-button-inner > .a-button-text').click()
+
+
+
+cy.get('.sc-action-delete-active > .a-declarative > .a-color-link').click()
+cy.contains(' foi removido de Carrinho de compras.').should('exist')
     });
 });
--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -6,15 +6,7 @@ describe('Busca na Amazon', () => {
             .type('blusa crochê inverno');
 
         cy.get('#nav-search-submit-button').click()
-        //Papito:
-        // cy.contains('label', 'Feminino')
-        // .find('input')
-        // .check()
-        // .should('be.checked')
-
-        //GPT:
-        // cy.get('#p_n_feature_twenty-eight_browse-bin\/121476056011 > .a-list-item > .a-link-normal > .a-checkbox > label > .a-icon')
-
+   
         cy.get('span.a-color-state.a-text-bold')
             .should('contain.text', 'blusa crochê inverno');
 
@@ -22,25 +14,7 @@ describe('Busca na Amazon', () => {
             .should('be.visible').click();
 
         cy.get('#productTitle').should('exist');
-        //  cy.contains('span', 'Moletom feminino térmico básico de crochê').click();
+   
     });
 
-    // it('Realiza uma busca inválida', () => {
-    //     cy.start()
-
-    //     cy.get('#twotabsearchtextbox').click()
-    //         .type('produtoinexistente1');
-
-    //     cy.get('#nav-search-submit-button').click();
-    //     cy.contains('span.a-size-medium.a-color-base.a-text-normal', 'produtoinexistente1')
-    //   .should('be.visible');        
-    // });
-
-});
-
-// cy.contains('resultados').should('exist');
-// cy.contains('span.menu-text', 'Soluções').click();
-// cy.contains('span.menu-text', 'Soluções', { timeout: 5000 }).trigger('mouseover');
-// cy.contains('span', 'Desenvolvimento de cursos EaD', { timeout: 5000 }).should('be.visible').click();
-
-//cy.get('span.menu-text').click()
+   });
